4dbcd6c8912890cf171860ca2dd6eb0772c865c6
Martin Yuan <myuan@fb.com>
2020-09-06 16:35:50 -0700

    Catch torchscript serialization error and terminate.
    ghstack-source-id: 91363c4a8048dddfbb25b3a04d296a1973ec88d5
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/44266

--- torch/csrc/jit/serialization/export_module.cpp
+++ torch/csrc/jit/serialization/export_module.cpp
@@ -1,6 +1,7 @@
 #include <torch/csrc/jit/serialization/export.h>
 
 #include <c10/util/Exception.h>
+#include <torch/csrc/Exceptions.h>
 #include <torch/csrc/jit/ir/attributes.h>
 #include <torch/csrc/jit/ir/ir.h>
 #include <torch/csrc/jit/ir/type_hashing.h>
@@ -267,24 +268,33 @@ class ScriptModuleSerializer {
       bool bytecode_format,
       bool save_mobile_debug_info) {
     C10_LOG_API_USAGE_ONCE("torch.script.save");
-    writeExtraFiles(module, extra_files);
-    // Serialize the model object
-    writeArchive("data", module._ivalue());
-    // Then we serialize all code info.
-    writeCode(module.type());
-    // The tensor constants from the code are written to a separate archive
-    // so loading the code does not depend on loading the data
-    std::vector<IValue> ivalue_constants(
-        constant_table_.begin(), constant_table_.end());
-    writeArchive("constants", c10::ivalue::Tuple::create(ivalue_constants));
-    if (bytecode_format) {
-      writeByteCode(module, save_mobile_debug_info);
-      writeMobileMetadata(module, extra_files);
-    }
 
-    // Acquires and sets minimum (dynamic) version
-    for (auto& item : file_streams_) {
-      writer_.setMinVersion(item.value().minVersion());
+    try {
+      writeExtraFiles(module, extra_files);
+      // Serialize the model object
+      writeArchive("data", module._ivalue());
+      // Then we serialize all code info.
+      writeCode(module.type());
+      // The tensor constants from the code are written to a separate archive
+      // so loading the code does not depend on loading the data
+      std::vector<IValue> ivalue_constants(
+          constant_table_.begin(), constant_table_.end());
+      writeArchive("constants", c10::ivalue::Tuple::create(ivalue_constants));
+      if (bytecode_format) {
+        writeByteCode(module, save_mobile_debug_info);
+        writeMobileMetadata(module, extra_files);
+      }
+
+      // Acquires and sets minimum (dynamic) version
+      for (auto& item : file_streams_) {
+        writer_.setMinVersion(item.value().minVersion());
+      }
+    } catch (c10::Error& error) {
+      std::cerr << "Error in module serializer: " << error.what() << std::endl;
+      std::terminate();
+    } catch (...) {
+      std::cerr << "Error in module serializer: " << std::endl;
+      std::terminate();
     }
   }
 
